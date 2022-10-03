1ab893dd44a88823088c2eb9a9aa4af7fbdeb112
Darkce <darkce97@gmail.com>
2022-10-03 20:01:08 +0800

    build: remove Makefile

--- build-wasm.sh
+++ build-wasm.sh
@@ -21,7 +21,14 @@
 set -eo pipefail
 
 SCRIPT_ROOT=$(dirname $0)/wasm/scripts
+DEPS_ROOT=$(dirname $0)/wasm/deps
 
-$SCRIPT_ROOT/build-flann.sh
-$SCRIPT_ROOT/build-boost.sh
+if [ ! -d "${DEPS_ROOT}/flann/build/lib" ]; then
+  $SCRIPT_ROOT/build-flann.sh
+fi
+
+if [ ! -d "${DEPS_ROOT}/boost/build/lib" ]; then
+  $SCRIPT_ROOT/build-boost.sh
+fi
+
 $SCRIPT_ROOT/build-pcl.sh
